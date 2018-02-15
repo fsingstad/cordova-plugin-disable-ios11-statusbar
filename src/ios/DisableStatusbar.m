058bf87951c3536db512307fd0a26efe44e3a73b
fsingstad <31435957+fsingstad@users.noreply.github.com>
2018-02-15 12:55:31 +0100

    Update DisableStatusbar.m

--- src/ios/DisableStatusbar.m
+++ src/ios/DisableStatusbar.m
@@ -6,15 +6,17 @@
 
 @implementation DisableStatusbar
 
+override func preferredScreenEdgesDeferringSystemGestures() -> UIRectEdge {
+  return .top
+}
+setNeedsUpdateOfScreenEdgesDeferringSystemGestures()
+    
 - (void)pluginInitialize
 {
 #if __IPHONE_OS_VERSION_MAX_ALLOWED >= 110000
     if (@available(iOS 11.0, *)) {
         [self.webView.scrollView setContentInsetAdjustmentBehavior:UIScrollViewContentInsetAdjustmentNever];
-        override func preferredScreenEdgesDeferringSystemGestures() -> UIRectEdge {
-          return .top
-        }
-        setNeedsUpdateOfScreenEdgesDeferringSystemGestures()
+
     }
 #endif
 }
